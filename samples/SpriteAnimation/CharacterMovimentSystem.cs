13e320f00e4ea2274d3e3c6b4a4f0701c48ac03a
Ronildo Souza <ronildo_rns@hotmail.com>
2020-05-23 02:26:13 -0300

    Create sprite animation sample

---
+++ samples/SpriteAnimation/CharacterMovimentSystem.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework.Input;
+using MonoGame.Helper.ECS;
+using MonoGame.Helper.ECS.Components.Drawables;
+using MonoGame.Helper.ECS.Components.Physics;
+using MonoGame.Helper.ECS.Systems;
+
+namespace SpriteAnimation
+{
+    [RequiredComponent(typeof(TransformComponent))]
+    [RequiredComponent(typeof(SpriteAnimationComponent))]
+    public class CharacterMovimentSystem : MonoGame.Helper.ECS.System, IUpdatable
+    {
+        public void Update()
+        {
+            var characterEntity = Scene.GetEntity("character");
+
+            var ks = Keyboard.GetState();
+
+            if (ks.IsKeyDown(Keys.Left))
+                HorizontalMove(ref characterEntity, 180, false);
+
+            if (ks.IsKeyDown(Keys.Up))
+                VerticalMove(ref characterEntity, 90, false);
+
+            if (ks.IsKeyDown(Keys.Right))
+                HorizontalMove(ref characterEntity, 270);
+
+            if (ks.IsKeyDown(Keys.Down))
+                VerticalMove(ref characterEntity, 0);
+        }
+
+        private void HorizontalMove(
+            ref Entity characterEntity,
+            int sourcePosY,
+            bool moveRight = true)
+        {
+            var spriteAnimationComponent = characterEntity.GetComponent<SpriteAnimationComponent>();
+            var transformComponent = characterEntity.GetComponent<TransformComponent>();
+            var tempPosition = characterEntity.Transform.Position;
+            var direction = moveRight ? 1 : -1;
+
+            var sourceRectangle = spriteAnimationComponent.SourceRectangle.Value;
+            sourceRectangle.Y = sourcePosY;
+
+            spriteAnimationComponent.IsPlaying = true;
+            spriteAnimationComponent.SourceRectangle = sourceRectangle;
+
+            tempPosition.X += (float)(transformComponent.Velocity.X * Scene.DeltaTime) * direction;
+
+            #region Out of screen in left or right
+            if (tempPosition.X + spriteAnimationComponent.FrameWidth < 0f)
+                tempPosition.X = Scene.ScreenWidth;
+
+            if (tempPosition.X > Scene.ScreenWidth)
+                tempPosition.X = -spriteAnimationComponent.FrameWidth;
+            #endregion
+
+            characterEntity.SetPosition(tempPosition);
+        }
+
+        private void VerticalMove(
+            ref Entity characterEntity,
+            int sourcePosY,
+            bool moveDown = true)
+        {
+            var spriteAnimationComponent = characterEntity.GetComponent<SpriteAnimationComponent>();
+            var transformComponent = characterEntity.GetComponent<TransformComponent>();
+            var tempPosition = characterEntity.Transform.Position;
+            var direction = moveDown ? 1 : -1;
+
+            var sourceRectangle = spriteAnimationComponent.SourceRectangle.Value;
+            sourceRectangle.Y = sourcePosY;
+
+            spriteAnimationComponent.IsPlaying = true;
+            spriteAnimationComponent.SourceRectangle = sourceRectangle;
+
+            tempPosition.Y += (float)(transformComponent.Velocity.Y * Scene.DeltaTime) * direction;
+
+            #region Out of screen in top or bottom
+            if (tempPosition.Y + spriteAnimationComponent.FrameHeight < 0f)
+                tempPosition.Y = Scene.ScreenHeight;
+
+            if (tempPosition.Y > Scene.ScreenHeight)
+                tempPosition.Y = -spriteAnimationComponent.FrameHeight;
+            #endregion
+
+            characterEntity.SetPosition(tempPosition);
+        }
+    }
+}
---
+++ samples/SpriteAnimation/Game1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Helper;
+using MonoGame.Helper.ECS;
+using MonoGame.Helper.ECS.Components.Drawables;
+using MonoGame.Helper.ECS.Components.Physics;
+
+namespace SpriteAnimation
+{
+    public class Game1 : GameCore
+    {
+        public Game1() : base() { }
+
+        protected override void Initialize()
+        {
+            var scene = new Scene()
+                .AddSystem<CharacterMovimentSystem>();
+
+            var texture = Content.Load<Texture2D>("character");
+
+            scene.CreateEntity("character")
+                .SetPosition(100, 100)
+                .AddComponent(new TransformComponent { Velocity = new Vector2(100) })
+                .AddComponent(new SpriteAnimationComponent(texture, 4, 4, 100, AnimateType.PerRow));
+            //.AddComponent(new SpriteComponent(texture));
+
+            SetScene(scene);
+        }
+    }
+}
