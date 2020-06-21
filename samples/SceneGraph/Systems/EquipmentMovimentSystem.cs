6f7901093094b88651d6c67c2d9994abd9e5158f
Ronildo Souza <ronildo_rns@hotmail.com>
2020-06-21 17:40:18 -0300

    Implement sample scene graph

---
+++ samples/SceneGraph/Systems/EquipmentMovimentSystem.cs
@@ -0,0 +1,25 @@
+﻿using MonoGame.Helper.ECS;
+using MonoGame.Helper.ECS.Systems;
+using SceneGraph.Components;
+using System.Linq;
+
+namespace SceneGraph.Systems
+{
+    [RequiredComponent(typeof(EquipmentComponent))]
+    public class EquipmentMovimentSystem : MonoGame.Helper.ECS.System, IUpdatable
+    {
+        public void Update()
+        {
+            var entities = Scene.GetEntities(_ => Matches(_));
+
+            for (int i = 0; i < entities.Count; i++)
+            {
+                var entity = entities.ElementAt(i);
+                var equipmentComponent = entity.GetComponent<EquipmentComponent>();
+                var newPosition = entity.Parent.Transform.Position - equipmentComponent.OffsetPosition;
+
+                entity.SetPosition(newPosition);
+            }
+        }
+    }
+}
