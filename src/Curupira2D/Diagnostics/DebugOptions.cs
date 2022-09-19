a4749f8dff8e9a369e801f64f43547e486c3f67b
Ronildo Souza <ronildo_rns@hotmail.com>
2022-09-18 23:45:42 -0300

    feat: implements debug options parameter

---
+++ src/Curupira2D/Diagnostics/DebugOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curupira2D.Diagnostics
+{
+    public class DebugOptions
+    {
+        public bool DebugActive { get; set; }
+        public bool DebugWithUICamera2D { get; set; }
+        public Color TextColor { get; set; } = Color.Black;
+    }
+}
