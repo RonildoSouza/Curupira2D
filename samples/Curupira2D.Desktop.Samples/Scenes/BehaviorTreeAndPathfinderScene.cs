4fce3295d738d92080ffdf5b2a51edfd47f1feb5
Ronildo Souza <ronildo_rns@hotmail.com>
2025-04-01 22:49:19 -0300

    wip

--- samples/Curupira2D.Desktop.Samples/Scenes/BehaviorTreeAndPathfinderScene.cs
+++ samples/Curupira2D.Desktop.Samples/Scenes/BehaviorTreeAndPathfinderScene.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using Curupira2D.AI.BehaviorTree;
+﻿using Curupira2D.AI.BehaviorTree;
 using Curupira2D.AI.Extensions;
+using Curupira2D.AI.Pathfinding;
 using Curupira2D.AI.Pathfinding.AStar;
-using Curupira2D.AI.Pathfinding.Graphs;
 using Curupira2D.Desktop.Samples.Common.Scenes;
 using Curupira2D.ECS;
 using Curupira2D.ECS.Components.Drawables;
 using Curupira2D.Extensions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Curupira2D.Desktop.Samples.Scenes
 {
@@ -20,11 +20,11 @@ namespace Curupira2D.Desktop.Samples.Scenes
         BehaviorTree _behaviorTree;
         Texture2D _minerTexture, _goldMineTexture, _pixelTexture;
         Entity _miner, _goldMine;
-        IEnumerable<Vector2> _pathtoScreen;
+        IEnumerable<Vector2> _closestGoldMineEdges;
 
         int _pathIndex = 0;
-        Vector2 _objectPosition;
-        Vector2 _targetPosition;
+        Vector2 _minerPosition;
+        Vector2 _nearestGoldMinePosition;
         readonly float _speed = 100f;
 
         public override void LoadContent()
@@ -36,91 +36,98 @@ namespace Curupira2D.Desktop.Samples.Scenes
             ShowControlTips("MOVIMENT: Keyboard Arrows", y: 120f);
 
 
-
-
             _minerTexture = GameCore.Content.Load<Texture2D>("AI/GoblinSpritesheet");
             _goldMineTexture = GameCore.Content.Load<Texture2D>("AI/GoldMineSpritesheet");
             _pixelTexture = GameCore.GraphicsDevice.CreateTextureRectangle(10, Color.Red * 0.5f);
 
-            _objectPosition = ScreenCenter;
+            _minerPosition = new Vector2(700, 500);
 
-            _miner = CreateEntity("miner", _objectPosition)
-                .AddComponent(new SpriteComponent(_minerTexture, sourceRectangle: new Microsoft.Xna.Framework.Rectangle(0, 0, 64, 64)));
+            _miner = CreateEntity("miner", _minerPosition)
+                .AddComponent(new SpriteComponent(_minerTexture, sourceRectangle: new Rectangle(0, 0, 64, 64)));
 
             _goldMine = CreateEntity("goldMine", 100, 200)
-               .AddComponent(new SpriteComponent(_goldMineTexture, sourceRectangle: new Microsoft.Xna.Framework.Rectangle(0, 0, 32, 32)));
+               .AddComponent(new SpriteComponent(_goldMineTexture, sourceRectangle: new Rectangle(0, 0, 32, 32)));
 
 
 
+            // PATHFINDING
+            var baseGridGraph = 20;
 
-            var gridGraph = new GridGraph(80, 64, true)
-            {
-                //Walls = walls,
-            };
+            var gridGraph = GridGraphBuilder.Build(ScreenWidth / baseGridGraph, ScreenHeight / baseGridGraph, true);
 
             var start = new System.Drawing.Point(
-                (int)_miner.Position.X / 10, (int)InvertPositionY(_miner.Position.Y) / 10);
+                (int)_miner.Position.X / baseGridGraph,
+                (int)InvertPositionY(_miner.Position.Y) / baseGridGraph);
+
             var goal = new System.Drawing.Point(
-                (int)_goldMine.Position.X / 10, (int)InvertPositionY(_goldMine.Position.Y) / 10);
+                (int)_goldMine.Position.X / baseGridGraph,
+                (int)InvertPositionY(_goldMine.Position.Y) / baseGridGraph);
+
 
             var path = AStarPathfinder.FindPath(gridGraph, start, goal);
-            _pathtoScreen = path.Edges.Select(_ => new Vector2(_.X * 10, InvertPositionY(_.Y * 10)));
+            _closestGoldMineEdges = path.Edges.Select(_ => PositionToScene(new Vector2(_.X * baseGridGraph, _.Y * baseGridGraph)));
 
-            _targetPosition = _pathtoScreen.ElementAt(_pathIndex);
+            _nearestGoldMinePosition = _closestGoldMineEdges.ElementAt(_pathIndex);
 
-
-            /*
-            Tired
-            Not Tired
-            Bag Empty
-            Bag Full
-            Mine End
-            Find Other Mine
-             */
-
-            _blackboard = new Blackboard();
-
-            _behaviorTree = BehaviorTreeBuilder.GetInstance()
-                .Selector()
-                    .DebugLogAction("Finding mine")
-                    .Sequence()
-                        .DebugLogAction("Fatigue?")
-                        .DebugLogAction("Go home")
-                        .DebugLogAction("Sleep")
-                    .Close()
-                    .Sequence()
-                        .DebugLogAction("Mine has gold?")
-                        .DebugLogAction("Miner with bag empty")
-                        .DebugLogAction("Deposit gold")
-                    .Close()
-                .Close()
-            .Build(_blackboard);
+            Debug.WriteLine(gridGraph.GetDebugPathfinder(start, goal, path, true));
 
 
-            Debug.WriteLine(_behaviorTree.GetTreeStructure());
+            ///*
+            //Tired
+            //Not Tired
+            //Bag Empty
+            //Bag Full
+            //Mine End
+            //Find Other Mine
+            // */
+
+            //_blackboard = new Blackboard();
+
+            //_behaviorTree = BehaviorTreeBuilder.GetInstance()
+            //    .Selector()
+            //        .DebugLogAction("Finding mine")
+            //        .Sequence()
+            //            .DebugLogAction("Fatigue?")
+            //            .DebugLogAction("Go home")
+            //            .DebugLogAction("Sleep")
+            //        .Close()
+            //        .Sequence()
+            //            .DebugLogAction("Mine has gold?")
+            //            .DebugLogAction("Miner with bag empty")
+            //            .DebugLogAction("Deposit gold")
+            //        .Close()
+            //    .Close()
+            //.Build(_blackboard);
+
+
+            //Debug.WriteLine(_behaviorTree.GetTreeStructure());
 
             base.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
-            if (_objectPosition != _targetPosition)
+            //Debug.WriteLine(_pathIndex);
+
+            if (_pathIndex < _closestGoldMineEdges.Count())
             {
-                var direction = _targetPosition - _objectPosition;
+                var direction = (_nearestGoldMinePosition - _minerPosition).GetSafeNormalize();
 
                 if (direction.Length() > 0)
                 {
-                    direction.Normalize();
-                    _objectPosition += direction * _speed * DeltaTime;
+                    _minerPosition += direction * _speed * DeltaTime;
+                    _miner.SetPosition(_minerPosition);
 
-                    if (Vector2.Distance(_objectPosition, _targetPosition) < 1f)
+                    // Next edge position without loop (index reset to zero)
+                    if (Vector2.Distance(_minerPosition, _nearestGoldMinePosition) < 1f && _pathIndex < _closestGoldMineEdges.Count() - 1)
                     {
-                        _objectPosition = _targetPosition;
-                        _miner.SetPosition(_objectPosition);
-
-                        _pathIndex = (_pathIndex + 1) % _pathtoScreen.Count();
-                        _targetPosition = _pathtoScreen.ElementAt(_pathIndex);
+                        _pathIndex = (_pathIndex + 1) % _closestGoldMineEdges.Count();
+                        _nearestGoldMinePosition = _closestGoldMineEdges.ElementAt(_pathIndex);
                     }
+
+                    // Finish position
+                    if (Vector2.Distance(_minerPosition, _nearestGoldMinePosition) < 1f)
+                        _minerPosition = _nearestGoldMinePosition;
                 }
             }
 
@@ -131,9 +138,9 @@ namespace Curupira2D.Desktop.Samples.Scenes
         {
             SpriteBatch.Begin();
 
-            //Draw edges for debug purposes.
-            for (int i = 0; i < _pathtoScreen.Count(); i++)
-                SpriteBatch.Draw(_pixelTexture, _pathtoScreen.Select(_ => new Vector2(_.X, InvertPositionY(_.Y))).ElementAt(i), Color.White);
+            // DRAW EDGES FOR DEBUG
+            for (int i = 0; i < _closestGoldMineEdges.Count(); i++)
+                SpriteBatch.Draw(_pixelTexture, _closestGoldMineEdges.Select(PositionToScene).ElementAt(i), Color.White);
 
             SpriteBatch.End();
 
